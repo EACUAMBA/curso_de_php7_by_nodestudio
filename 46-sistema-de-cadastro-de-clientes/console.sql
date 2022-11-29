c625208cff7745cb821d089eebc41da0e3755715
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-11-29 13:01:02 +0200

    Criei o projecto, adicionei algumas dependências, adicionei uma tabela dos clientes com os botões de gerenciament, adicionei o SQL com a criação da base de dados e etc.

---
+++ 46-sistema-de-cadastro-de-clientes/console.sql
@@ -0,0 +1,12 @@
+create database if not exists curso_php7_by_nodestudio;
+use curso_php7_by_nodestudio;
+
+create table if not exists `cliente`
+(
+    id        bigint       not null auto_increment primary key,
+    nome      varchar(255) not null,
+    sobrenome varchar(255) not null,
+    email     varchar(255) null unique,
+    idade     int(3)       null
+);
+
